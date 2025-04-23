15276ad698bdf8b69ffaddda2f8c299ba24d917a
Farid0630 <161107771+Farid0630@users.noreply.github.com>
2025-04-24 05:28:21 +0800

    fitur login dengan authentifikasi google

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body class="bg-light">
+    <main class="container vh-100 d-flex justify-content-center align-items-center">
+        <div class="col-6 mx-auto text-center px-5 py-5 border rounded bg-white">
+            @if (Session::has('error'))
+                <div class="alert alert-danger" role="alert">
+                    {{ Session::get('error') }}
+                </div>
+            @endif
+            <h1>Login</h1>
+            <p>Silakan login dengan Akun Google yang kamu punya</p>
+            <a href="{{ url('auth/redirect') }}" class="btn border border-primary">
+                <i class="bi bi-google me-2"></i> Login dengan Google
+            </a>
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +20,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/redirect', function () {
-    return Socialite::driver('google')->redirect();
-});
+Route::redirect('home', 'dashboard');
 
-Route::get('/auth/callback', function () {
-    $user = Socialite::driver('google')->user();
+Route::get('/auth', [authController::class, 'index'])->name('login')->middleware('guest');
 
-    // $user->token;
-});
+Route::get('/auth/redirect', [authController::class, 'redirect'])->middleware('guest');
+
+Route::get('/auth/callback', [authController::class, 'callback'])->middleware('guest');
+
+Route::get('/auth/logout', [authController::class, 'logout']);
+
+Route::get('/dashboard', function () {
+    return 'Selamat datang '.Auth::user()->email.' di dashboard';
+})->middleware('auth'); ;
